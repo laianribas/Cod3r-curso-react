71690e3f91a0e1825ea8e9c2a61ac26970f07a91
laianribas <laian67a@gmail.com>
2022-01-02 19:45:55 -0300

    Criação de layout e ajustes dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+import './App.css'
 import React from 'react'
 
 import Primeiro from './components/Basicos/Primeiro'
 import ComParametro from './components/Basicos/ComParametro'
 import Fragmento from './components/Basicos/Fragmento'
 import NumeroAleatorio from './components/Basicos/NumeroAleatorio'
+import Card from './components/layout/Card'
 
 export default () => {
   return (
-    <div id="app">
+    <div className="App">
       <h1>Fundamentos React 2</h1>
-      <NumeroAleatorio max={10} min={0} />
-      <Fragmento />
-      <ComParametro titulo="Nota do Aluno" aluno="Pedro" nota={8.5} />
-      <Primeiro />
+      <div className="Wrapper">
+        <Card titulo="#04 - Desafio Número Aleatório">
+          <NumeroAleatorio max={50} min={0} />
+        </Card>
+        <Card titulo="#03 - Fragment">
+          <Fragmento />
+        </Card>
+        <Card titulo="#02 - Com Paramentro">
+          <ComParametro titulo="Nota do Aluno" aluno="Pedro" nota={8.5} />
+        </Card>
+        <Card titulo="#01 - Primeiro Componente">
+          <Primeiro />
+        </Card>
+      </div>
     </div>
   )
 }
--- src/components/Basicos/NumeroAleatorio.jsx
+++ src/components/Basicos/NumeroAleatorio.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
 export default props => {
-  return <h2>{Math.random() * (props.max - props.min) + props.min}</h2>
+  return (
+    <div>
+      <h2>Valor aleatório</h2>
+      <p>
+        <strong>Valor Mínimo</strong> {props.min}
+      </p>
+      <p>
+        <strong>Valor Máximo</strong> {props.max}
+      </p>
+      <p>
+        <strong>
+          {parseInt(Math.random() * (props.max - props.min) + props.min)}
+        </strong>
+      </p>
+    </div>
+  )
 }
