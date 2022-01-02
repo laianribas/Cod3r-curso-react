eea28a663728bab05f9d791c0e30aa5f33964a9c
laianribas <laian67a@gmail.com>
2022-01-02 10:10:34 -0300

    Primeiro JSX Com Parametros

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react'
 import './index.css'
 
 import Primeiro from './components/Basicos/Primeiro'
+import ComParametro from './components/Basicos/ComParametro'
 
 const element = document.getElementById('root')
 ReactDOM.render(
-  <div>
+  <div id="app">
     <Primeiro />
+    <ComParametro titulo="Nota do Aluno" aluno="Pedro" nota="8,5" />
+    <ComParametro titulo="Nota do Aluno" aluno="Maria" nota="8" />
   </div>,
   element
 )
