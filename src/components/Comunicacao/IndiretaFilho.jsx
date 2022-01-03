884f5c0fb776ea47ab6b7c99f4b4eba16313c557
laianribas <laian67a@gmail.com>
2022-01-03 18:04:58 -0300

    Valores aleatórios

--- src/components/Comunicacao/IndiretaFilho.jsx
+++ src/components/Comunicacao/IndiretaFilho.jsx
@@ -2,10 +2,10 @@
 import React from "react";
 
 export default (props) => {
-    let min = 50;
-    let max = 100;
-    const idade = () => parseInt(Math.random()*(max - min) + min);
-    const nerd = () => Math.random() > 0.5
+  let min = 50;
+  let max = 100;
+  const idade = () => parseInt(Math.random() * (max - min) + min);
+  const nerd = () => Math.random() > 0.5;
   return (
     <div>
       <div>Filho</div>
